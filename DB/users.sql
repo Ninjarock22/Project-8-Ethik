f771ddb59185c820a287bcef14552788c823c624
Philipp Uhlendorf <philipp.uhlendorf@gmx.de>
2025-03-26 21:07:39 +0100

    Database fix

--- DB/users.sql
+++ DB/users.sql
@@ -7,64 +7,17 @@
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE [users] (
+  [id] int NOT NULL IDENTITY(1,1),
+  [name] varchar(75) NOT NULL,
+  [password] varchar(255) NOT NULL,
+  [email] varchar(100) NOT NULL,
+  [age] int DEFAULT NULL,
+  [status] tinyint(4) NOT NULL DEFAULT 0,
+  PRIMARY KEY ([id]),
+  UNIQUE ([email])
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Datenbank: `ethik-project`
---
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(75) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `age` int(100) DEFAULT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Daten für Tabelle `users`
---
 
 INSERT INTO `users` (`id`, `name`, `password`, `email`, `age`, `status`) VALUES
 (6, 'Test', '$2y$10$/QKUV9ozhBvVpShsNZAaauHt8mGEy5KdxOy8bljufSncvY8eMkleW', 'test@test.de', NULL, 0),
-
---
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
