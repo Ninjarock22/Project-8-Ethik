cc229e3da02770625fae8a92b0a18c7a59601d09
Ninjarock22 <behj28@gmail.com>
2025-03-31 22:00:14 +0200

    bugfixes

--- authsystem/register.php
+++ authsystem/register.php
@@ -65,8 +65,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
     </head>
     <body>
         <header>
-            <!-- Popup Menu Icon -->
-            <div id="popup-menu" class="popup-menu">
+            <div class="popup-icon" onclick="togglePopupMenu()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
+                    <line x1="3" y1="12" x2="21" y2="12"></line>
+                    <line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg>
+            </div>
+            <button class="login-button" onclick="window.location.href='login.php';">Login</button>
+        </header>
+        <!-- Popup Menu Icon -->
+        <div id="popup-menu" class="popup-menu">
             <div class="card3">
                 <ul class="list">
                     <li class="element">
