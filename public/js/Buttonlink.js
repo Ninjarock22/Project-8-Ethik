95b768c91530b209ee63ebe02ecc927bd3b3a572
Philipp Uhlendorf <pu-ol@gmx.de>
2025-04-13 22:25:10 +0200

    Fix

--- public/js/Buttonlink.js
+++
@@ -1,5 +0,0 @@
-// Warum nicht einfach direkt in die Entsprechende Datei schreiben?
-// Das sind nur 3 Zeilen Code
-document.getElementById('navigate').addEventListener('click', function () {
-    window.location.href = '../docs/readme.pdf';
-});
---
+++ public2/js/Buttonlink.js
@@ -0,0 +1,5 @@
+// Warum nicht einfach direkt in die Entsprechende Datei schreiben?
+// Das sind nur 3 Zeilen Code
+document.getElementById('navigate').addEventListener('click', function () {
+    window.location.href = '../docs/readme.pdf';
+});
