13089ee2b14833c9c4ab6cbe5a4c3ea3dfba4249
Philipp Uhlendorf <pu-ol@gmx.de>
2025-04-13 17:32:37 +0200

    Fix Issue #29
    Signed-off-by: Philipp Uhlendorf <pu-ol@gmx.de>

--- authsystem/register.php
+++ authsystem/register.php
@@ -45,7 +45,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
         }
         $_SESSION['success'] = $success;
         $_SESSION['error'] = $error;
-        header("Location: ../authsystem/login.php");
         $query->close();
     }
     if (isset($insertQuery)) {
@@ -176,13 +175,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
                                     </div>
                                 </div>
                                 <br>
-                                <?php echo isset($success) ? $success : ''; 
-                                    echo $error; 
-                                    if($result = $success){   
-                                    header("Refresh:1; url= ../authsystem/login.php");
-                                    }
-                                    ?>
+                                <div class="message">
+                                    <div id="message" style="color: green;">
+                                        <?php if (!empty($success)) echo $success; ?>
+                                    </div>
+                                    <div id="message">
+                                        <?php if (!empty($error)) echo $error; ?>
+                                    </div>
                                 </div>
+                                <?php
+                                if (!empty($success)) {
+                                    echo '<script>
+                                        setTimeout(function() {
+                                            window.location.href = "../authsystem/login.php";
+                                        }, 2000);
+                                    </script>';
+                                }
+                                ?>
                             </form>
                         </div>
                     </div>
