72a43cd08b52d0a253a5a3236080b26675dfc8a4
Philipp Uhlendorf <pu-ol@gmx.de>
2025-04-24 21:57:49 +0200

    Update logout.php

--- authsystem/logout.php
+++ authsystem/logout.php
@@ -51,7 +51,7 @@
 <?php
 session_start();
 if(session_destroy()){
-    header("Refresh:1;url=../authsystem/index.php");//Refresh:2;
+    header("Refresh:1;url=index.php");//Refresh:2;
     exit;
 }
 ?>
